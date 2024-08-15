ad09472c3e56bca6be9c08d19b266a6247b6af62
appanikaushik <appani.kaushik@bajajfinserv.in>
2024-08-15 19:14:25 +0530

    motoko first commit

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,43 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor Dbank {
+  stable var currentValue :Float = 300;
+  stable var startTime = Time.now();
+
+  Debug.print(debug_show(startTime));
+  // currentValue := 0;
+
+  public func checkBal () : async Float { 
+    return currentValue;
   };
-};
+
+  let _id = 123123123123; // let is constant 
+  // Debug.print(debug_show(currentValue));
+  // Debug.print(debug_show(id));
+
+
+  public func topUp(amount: Float){
+    currentValue+=amount;
+    Debug.print(debug_show(currentValue));
+  };
+
+    public func withDraw(amount: Float){
+    let temp: Float = currentValue - amount;
+    if(temp >=0){
+      currentValue-=amount;
+      Debug.print(debug_show(currentValue));
+    }else{
+       Debug.print("Low Balance");
+    }
+  };
+
+  public func compound(){
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS/1000000000;
+    currentValue := currentValue * (0.01 ** Float.fromInt(timeElapsedS));
+    startTime := currentTime;
+  }
+}
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,37 @@
 import { dbank } from "../../declarations/dbank";
 
+window.addEventListener("load", async() => {
+    await fetchBal();
+})
+
+const fetchBal = async() => {
+    try {
+        const currentBal = await dbank.checkBal();
+        console.log(currentBal);
+        document.getElementById("value").innerText = currentBal;
+    } catch (error) {
+        console.error("Error during query:", error);
+    }
+}
+
 document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+    e.preventDefault();
+    const button = document.querySelector("#submit-btn");
+    console.log(button);
+    button.setAttribute("disabled", true);
+    button.classList.add("disabled");
+    console.log(button);
+    const inputAmount = parseFloat(document.getElementById("input-amount").value);
+    const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+    button.removeAttribute("disabled");
+    if (document.getElementById("input-amount").value.length != 0) {
+        await dbank.topUp(inputAmount);
+        await fetchBal();
+    }
 
-  const name = document.getElementById("name").value.toString();
+    if (document.getElementById("withdrawal-amount").value.length != 0) {
+        await dbank.withDraw(outputAmount);
+        await fetchBal();
+    }
 
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+})
